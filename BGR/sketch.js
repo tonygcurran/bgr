97de150612d2b5bfb05ba80393a8ec97f3992bff
tonygcurran <62523322+tonygcurran@users.noreply.github.com>
2020-03-29 11:52:29 +1100

    Update sketch.js

--- BGR/sketch.js
+++ BGR/sketch.js
@@ -1,7 +1,7 @@
 
 function setup() {
   noCursor();
-  createCanvas(displayWidth, displayHeight);
+  createCanvas(windowWidth, windowHeight);
   //background(random(red,green,blue));
   x = random(5,height/4);
   y = random(5,height/4);
